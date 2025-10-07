52dc6611a239d6664d742611ac062031e78ea102
Deepak Kumar <deepakdagur0007@gmail.com>
2025-10-07 13:30:07 +0530

    learned creating element by html->javascitp-> react

---
+++ App.js
@@ -0,0 +1,12 @@
+const heading = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", { id: "heading" }, "This is Heading"),
+    React.createElement("h2", { id: "heading2" }, "This is Heading2"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", { id: "heading" }, "This is Heading"),
+    React.createElement("h2", { id: "heading2" }, "This is Heading2"),
+  ]),
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
